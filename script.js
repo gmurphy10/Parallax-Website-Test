00b4fd856d35227d804eb219447e0ef05d781233
george murphy <georgeolivermurphy@hotmail.com>
2021-05-27 17:15:03 +0100

    scroll layered on top

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let timeline = new TimelineMax();
 timeline
   .to('.rock', 3, {y: -300})
   .to('.girl', 3, {y: -200}, '-=3')
-  .to('.bg1', 3, { y: 50 }, "-=3");
+  .to('.bg1', 3, { y: 50 }, "-=3")
+  .to('.content', 3, { top: "0%" }, "-=10");
 
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
@@ -14,7 +15,9 @@ let scene = new ScrollMagic.Scene({
   // where does the animation start
   triggerHook: 0,
 })
-
+  // hookup animation to scene
   .setTween(timeline)
-  .setPin()
+  // Pin page on top of container image
+  .setPin("section")
+  // connect to controller
   .addTo(controller);
